cfdd642560aeb8585d2046cb267e2f1a4a8ae512
prem-kaithwas24 <prem.kaithwas@joshsoftware.com>
2025-05-20 15:03:56 +0530

    initial commit

---
+++ module-1/say-hello/src/App.tsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Bucket List</h1>
+      <ul>
+        <li>
+          <input type="checkbox" />
+          <label>Learn React</label>
+        </li>
+        <li>
+          <input type="checkbox" />
+          <label>Learn React</label>
+        </li>
+        <li>
+          <input type="checkbox" />
+          <label>Learn React</label>
+        </li>
+      </ul>
+    </>
+  )
+}
+
+export default App
